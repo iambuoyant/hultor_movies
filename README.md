a897dd5e7bd9d2c730b8e260129df9db119ed6d6
iambuoyant <imkhaaan@gmail.com>
2018-12-30 05:52:02 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hultor_movies
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="row">
+        <div className="col-sm-12">
+          Copyright &copy; 2018 Inam U Khan. All rights reserved
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
